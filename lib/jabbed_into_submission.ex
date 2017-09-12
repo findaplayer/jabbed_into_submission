ff09532d99bcfe4937ed3065881fd6f89d8355a0
John Hamelink <john@johnhamelink.com>
2017-09-12 18:23:05 +0100

    Remove cruft

--- lib/jabbed_into_submission.ex
+++ lib/jabbed_into_submission.ex
@@ -2,17 +2,4 @@ defmodule JabbedIntoSubmission do
   @moduledoc """
   Documentation for JabbedIntoSubmission.
   """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> JabbedIntoSubmission.hello
-      :world
-
-  """
-  def hello do
-    :world
-  end
 end
